5eb7272ee0de3aa47bc2781514b6eea136c39d70
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-01-09 19:20:27 -0600

    Create BunnyEars2.java

---
+++ recursion_1/BunnyEars2.java
@@ -0,0 +1,44 @@
+/**
+ * We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..)
+ * have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because
+ * they each have a raised foot. Recursively return the number of "ears" in the bunny
+ * line 1, 2, ... n (without loops or multiplication).
+ * <ul>
+ *  <li>bunnyEars2(0) → 0
+ *  <li>bunnyEars2(1) → 2
+ *  <li>bunnyEars2(2) → 5
+ * </ul>
+ * 
+ * @author LZ-FSDev
+ * @see https://codingbat.com/prob/p107330
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public class BunnyEars2 {
+    public static void main(String[] args) {
+        System.out.println("bunnyEars2(0) -> " + bunnyEars2(0));
+        System.out.println("bunnyEars2(1) -> " + bunnyEars2(1));
+        System.out.println("bunnyEars2(2) -> " + bunnyEars2(2));
+    }
+
+    /**
+     * We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..)
+     * have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because
+     * they each have a raised foot. Recursively return the number of "ears" in the bunny
+     * line 1, 2, ... n (without loops or multiplication).
+     * 
+     * @param bunnies the amount of bunnies in the line for adding the number of ears
+     * @return number of ears from the bunnies in line
+     * @since 0.0.1
+     */
+    public static int bunnyEars2(int bunnies) {
+        if (bunnies == 0) { 
+            return 0;
+        }
+        if (bunnies % 2 == 0) {
+            return (bunnyEars2(bunnies-1) + 3);
+        } else {
+            return (bunnyEars2(bunnies-1) + 2);
+        }
+    }
+}
