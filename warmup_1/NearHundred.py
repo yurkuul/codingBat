73041f167b8ee22d45e73270bd1f07ef51486df8
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-03-04 19:46:53 -0600

    Create NearHundred.py

---
+++ warmup_1/NearHundred.py
@@ -0,0 +1,14 @@
+'''
+Given an int n, return True if it is within 10 of 100 or 200. Note: abs(num)
+computes the absolute value of a number.
+near_hundred(93) → True
+near_hundred(90) → True
+near_hundred(89) → False
+'''
+
+def near_hundred(n):
+    return abs(100-n) <= 10 or abs(200-n) <= 10
+
+print("near_hundred(93) -> " + str(near_hundred(93)))
+print("near_hundred(90) -> " + str(near_hundred(90)))
+print("near_hundred(89) -> " + str(near_hundred(89)))
