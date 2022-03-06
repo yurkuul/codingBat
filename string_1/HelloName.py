180ae8b9d62a5d43c99360b1db2c51c2a129f4e0
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-03-06 10:53:46 -0600

    Create HelloName.py

---
+++ string_1/HelloName.py
@@ -0,0 +1,13 @@
+'''
+Given a string name, e.g. "Bob", return a greeting of the form "Hello Bob!".
+hello_name('Bob') → 'Hello Bob!'
+hello_name('Alice') → 'Hello Alice!'
+hello_name('X') → 'Hello X!'
+'''
+
+def hello_name(name):
+    return "Hello " + name + "!"
+
+print("hello_name('Bob') -> " + str(hello_name('Bob')))
+print("hello_name('Alice') -> " + str(hello_name('Alice')))
+print("hello_name('X') -> " + str(hello_name('X')))
