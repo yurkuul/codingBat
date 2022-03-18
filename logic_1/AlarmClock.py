b16edb9c901b23b4657f2f720efbb0f2c26cfb73
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-03-18 15:21:46 -0500

    Create AlarmClock.py

---
+++ logic_1/AlarmClock.py
@@ -0,0 +1,29 @@
+'''
+Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a boolean
+indicating if we are on vacation, return a string of the form "7:00" indicating
+when the alarm clock should ring. Weekdays, the alarm should be "7:00" and on
+the weekend it should be "10:00". Unless we are on vacation -- then on weekdays
+it should be "10:00" and weekends it should be "off".
+alarm_clock(1, False) → '7:00'
+alarm_clock(5, False) → '7:00'
+alarm_clock(0, False) → '10:00'
+
+@author LZ-FSDev
+@see https://codingbat.com/prob/p119867
+@since 3.10.2
+@version 0.0.1
+'''
+
+def alarm_clock(day, vacation):
+    if vacation:
+        if day >= 1 and day <= 5:
+            return "10:00"
+        return "off"
+    else:
+        if day >= 1 and day <= 5:
+            return "7:00"
+        return "10:00"
+    
+print("alarm_clock(1, False) -> " + str(alarm_clock(1, False)))
+print("alarm_clock(5, False) -> " + str(alarm_clock(5, False)))
+print("alarm_clock(0, False) -> " + str(alarm_clock(0, False)))
