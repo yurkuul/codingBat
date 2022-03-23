b81e4df05d86305c35b78c87ad2e3220ec499638
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-03-22 19:06:48 -0500

    Create NearTen.py

---
+++ logic_1/NearTen.py
@@ -0,0 +1,15 @@
+'''
+Given a non-negative number "num", return True if num is within 2 of a multiple
+of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
+
+near_ten(12) → True
+near_ten(17) → False
+near_ten(19) → True
+'''
+
+def near_ten(num):
+    return num % 10 <= 2 or num % 10 >= 8
+
+print("near_ten(12) -> " + str(near_ten(12)))
+print("near_ten(17) -> " + str(near_ten(17)))
+print("near_ten(19) -> " + str(near_ten(19)))
