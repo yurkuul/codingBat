3a7e1da05123e0b02143c291f8efabc99bba77c2
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-03-26 19:24:36 -0500

    Create EndOther.py

---
+++ string_2/EndOther.py
@@ -0,0 +1,20 @@
+'''
+Given two strings, return True if either of the strings appears at the very end
+of the other string, ignoring upper/lower case differences (in other words, the
+computation should not be "case sensitive"). Note: s.lower() returns the
+lowercase version of a string.
+end_other('Hiabc', 'abc') → True
+end_other('AbC', 'HiaBc') → True
+end_other('abc', 'abXabc') → True
+'''
+
+def end_other(a, b):
+    return (a.lower().startswith(b.lower()) or a.lower().endswith(b.lower())) or (b.lower().startswith(a.lower()) or b.lower().endswith(a.lower()))
+    
+print("end_other('ab', 'ab12') -> " + str(end_other('ab', 'ab12')))
+print("end_other('Hiabc', 'abc') -> " + str(end_other('Hiabc', 'abc')))
+print("end_other('AbC', 'HiaBc') -> " + str(end_other('AbC', 'HiaBc')))
+print("end_other('abc', 'abXabc') -> " + str(end_other('abc', 'abXabc')))
+print("end_other('AbC', 'HiaBc') -> " + str(end_other('AbC', 'HiaBc')))
+print("end_other('Z', '12xz') -> " + str(end_other('Z', '12xz')))
+print("end_other('ab', 'ab12') -> " + str(end_other('ab', 'ab12')))
