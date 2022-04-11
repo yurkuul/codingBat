edb4e2d1a15f805aa469972e5855e2557065cf22
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-04-11 15:49:47 -0500

    Update ScoresSpecial.java

--- ap_1/ScoresSpecial.java
+++ ap_1/ScoresSpecial.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Given two arrays, A and B, of non-negative int scores. A "special" score is
  * one which is a multiple of 10, such as 40 or 90. Return the sum of largest
@@ -14,11 +16,25 @@
  * @author LZ-FSDev
  * @see https://codingbat.com/prob/p140485
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 public class ScoresSpecial {
     public static void main(String[] args) {
-        
+        int[] array1 = {12, 10, 4};
+        int[] array2 = {2, 20, 30};
+        int[] array3 = {20, 10, 4};
+        int[] array4 = {2, 20, 10};
+        int[] array5 = {12, 11, 4};
+        int[] array6 = {2, 20, 31};
+        System.out.println("scoresSpecial(" + Arrays.toString(array1) + ", "
+                            + Arrays.toString(array2) + ") -> "
+                            + scoresSpecial(array1, array2));
+        System.out.println("scoresSpecial(" + Arrays.toString(array3) + ", "
+                            + Arrays.toString(array4) + ") -> "
+                            + scoresSpecial(array3, array4));
+        System.out.println("scoresSpecial(" + Arrays.toString(array5) + ", "
+                            + Arrays.toString(array6) + ") -> "
+                            + scoresSpecial(array5, array6));
     }
 
     /**
@@ -29,13 +45,13 @@ public class ScoresSpecial {
      * largest special score in an array. Write your helper method after your
      * scoresSpecial() method in the JavaBat text area.
      * 
-     * @param a
-     * @param b
-     * @return
-     * @since 0.0.1
+     * @param a Array of scores.
+     * @param b Array of scores.
+     * @return The sum of the special scores in a and b.
+     * @since 0.0.2
      */
     public static int scoresSpecial(int[] a, int[] b) {
-        return 0;
+        return findSpecialScore(a) + findSpecialScore(b);
     }
 
     /**
