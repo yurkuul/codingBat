b9ac840874d5efddfa27924953de308492deb676
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-05-06 21:02:54 -0500

    Create Make_pi.py

---
+++ list_1/Make_pi.py
@@ -0,0 +1,13 @@
+'''
+Return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+make_pi() → [3, 1, 4]
+
+author LZ-FSDev
+see https://codingbat.com/prob/p113659
+'''
+
+def make_pi():
+    pi = [3, 1, 4]
+    return pi
+
+print("make_pi() -> " + str(make_pi()))
