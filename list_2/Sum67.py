6788a9b25b7aff9b0a7107ff1060a6b0270a58a1
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-05-15 15:04:45 -0500

    Create Sum67.py

---
+++ list_2/Sum67.py
@@ -0,0 +1,33 @@
+'''
+Return the sum of the numbers in the array, except ignore sections of numbers
+starting with a 6 and extending to the next 7 (every 6 will be followed by at
+least one 7). Return 0 for no numbers.
+sum67([1, 2, 2]) → 5
+sum67([1, 2, 2, 6, 99, 99, 7]) → 5
+sum67([1, 1, 6, 7, 2]) → 4
+
+author LZ-FSDev
+see https://codingbat.com/prob/p108886
+'''
+
+def sum67(nums):
+    index = 0
+    total = 0
+    inBetween = False
+    while (index < len(nums)):
+        if (nums[index] == 6):
+            inBetween = True
+        if (not inBetween):
+            total += nums[index]
+        if (nums[index] == 7):
+            inBetween = False
+        index += 1
+    return total
+
+array1 = [1, 2, 2]
+array2 = [1, 2, 2, 6, 99, 99, 7]
+array3 = [1, 1, 6, 7, 2]
+
+print("sum67(" + str(array1) + ") -> " + str(sum67(array1)))
+print("sum67(" + str(array2) + ") -> " + str(sum67(array2)))
+print("sum67(" + str(array3) + ") -> " + str(sum67(array3)))
