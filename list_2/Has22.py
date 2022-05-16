ffc0db6e943a2b9f815393e7113c10b43ad5b0f0
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-05-16 18:02:20 -0500

    Create Has22.py

---
+++ list_2/Has22.py
@@ -0,0 +1,24 @@
+'''
+Given an array of ints, return True if the array contains a 2 next to a 2
+somewhere.
+has22([1, 2, 2]) → True
+has22([1, 2, 1, 2]) → False
+has22([2, 1, 2]) → False
+
+author LZ-FSDev
+see https://codingbat.com/prob/p119308
+'''
+
+def has22(nums):
+    for i in range (len(nums)-1):
+        if (nums[i] == 2 and nums[i+1] == 2):
+            return True
+    return False
+
+array1 = [1, 2, 2]
+array2 = [1, 2, 1, 2]
+array3 = [2, 1, 2]
+
+print("has22(" + str(array1) + ") -> " + str(has22(array1)))
+print("has22(" + str(array2) + ") -> " + str(has22(array2)))
+print("has22(" + str(array3) + ") -> " + str(has22(array3)))
