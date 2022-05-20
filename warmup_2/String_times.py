e9d2e957f0ce5775ca0c5fe0887c40d925f5b9d3
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-05-19 19:55:58 -0500

    Create String_times.py

---
+++ warmup_2/String_times.py
@@ -0,0 +1,18 @@
+'''
+Given a string and a non-negative int n, return a larger string that is n
+copies of the original string.
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+
+author LZ-FSDev
+see https://codingbat.com/prob/p193507
+'''
+
+def string_times(str, n):
+    return str*n
+
+print("string_times('Hi', 2) -> " + string_times('Hi', 2))
+print("string_times('Hi', 3) -> " + string_times('Hi', 3))
+print("string_times('Hi', 1) -> " + string_times('Hi', 1))
