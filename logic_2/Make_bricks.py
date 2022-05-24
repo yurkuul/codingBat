85196d68bd09228cc6f65d10a4f9659ea786dc77
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-05-24 16:54:37 -0500

    Create Make_bricks.py

---
+++ logic_2/Make_bricks.py
@@ -0,0 +1,21 @@
+'''
+We want to make a row of bricks that is goal inches long. We have a number of
+small bricks (1 inch each) and big bricks (5 inches each). Return True if it is
+possible to make the goal by choosing from the given bricks. This is a little
+harder than it looks and can be done without any loops.
+
+make_bricks(3, 1, 8) → True
+make_bricks(3, 1, 9) → False
+make_bricks(3, 2, 10) → True
+
+author LZ-FSDev
+see https://codingbat.com/prob/p118406
+'''
+
+def make_bricks(small, big, goal):
+    bigAmount = min(big, goal/5)
+    return bigAmount*5 + small >= goal
+
+print("make_bricks(3, 1, 8) -> " + str(make_bricks(3, 1, 8)))
+print("make_bricks(3, 1, 9) -> " + str(make_bricks(3, 1, 9)))
+print("make_bricks(3, 2, 10) -> " + str(make_bricks(3, 2, 10)))
