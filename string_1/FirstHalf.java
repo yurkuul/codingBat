19b47f254df945201d5dedb94fc62de00040135e
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-06-23 22:13:27 -0500

    Create FirstHalf.java

---
+++ string_1/FirstHalf.java
@@ -0,0 +1,34 @@
+/**
+ * Given a string of even length, return the first half. So the string "WooHoo"
+ * yields "Woo".
+ * 
+ * <ul>
+ *  <li>firstHalf("WooHoo") → "Woo"
+ *  <li>firstHalf("HelloThere") → "Hello"
+ *  <li>firstHalf("abcdef") → "abc"
+ * </ul>
+ * 
+ * @author LZ-FSDev
+ * @see https://codingbat.com/prob/p172267
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public class FirstHalf {
+    public static void main(String[] args) {
+        System.out.println("firstHalf(\"WooHoo\") -> " + firstHalf("WooHoo"));
+        System.out.println("firstHalf(\"HelloThere\") -> " + firstHalf("HelloThere"));
+        System.out.println("firstHalf(\"abcdef\") -> " + firstHalf("abcdef"));
+    }
+    
+    /**
+     * Given a string of even length, return the first half. So the string
+     * "WooHoo" yields "Woo".
+     * 
+     * @param str String used to get the first half to return.
+     * @return The first half of str.
+     * @since 0.0.1
+     */
+    public static String firstHalf(String str) {
+        return str.substring(0, str.length()/2);
+    }    
+}
