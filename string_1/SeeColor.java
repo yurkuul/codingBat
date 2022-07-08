558487d171cf0c7629ef4558e5744eb111848f2a
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-07-07 20:52:55 -0500

    Create SeeColor.java

---
+++ string_1/SeeColor.java
@@ -0,0 +1,44 @@
+/**
+ * Given a string, if the string begins with "red" or "blue" return that color
+ * string, otherwise return the empty string.
+ * 
+ * <ul>
+ *  <li>seeColor("redxx") → "red"
+ *  <li>seeColor("xxred") → ""
+ *  <li>seeColor("blueTimes") → "blue"
+ * </ul>
+ * 
+ * @author LZ-FSDev
+ * @see https://codingbat.com/prob/p199216
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public class SeeColor {
+    public static void main(String[] args) {
+        System.out.println("seeColor(\"redxx\") -> " + seeColor("redxx"));
+        System.out.println("seeColor(\"xxred\") -> " + seeColor("xxred"));
+        System.out.println("seeColor(\"blueTimes\") -> "
+                            + seeColor("blueTimes"));
+    }
+
+    /**
+     * Given a string, if the string begins with "red" or "blue" return that
+     * color string, otherwise return the empty string.
+     * 
+     * @param str A String.
+     * @return "red" or "blue" if one of the colours starts at the beginning
+     * of str.
+     * @since 0.0.1
+     */
+    public static String seeColor(String str) {
+        if (str.length() < 3) {
+            return "";
+        }
+        if (str.substring(0, 3).equals("red")) {
+            return "red";
+        } else if (str.length() >= 4 && str.substring(0,4).equals("blue")) {
+            return "blue";
+        }
+        return "";
+    }
+}
