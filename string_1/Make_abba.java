4292ec0d3dc55c88189d254f3cd8faced37b0403
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-07-12 12:15:35 -0500

    Update Make_abba.java

--- string_1/Make_abba.java
+++ string_1/Make_abba.java
@@ -1,1 +1,35 @@
+/**
+ * Given two strings, a and b, return the result of putting them together in
+ * the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+ * 
+ * <ul>
+ *  <li>makeAbba("Hi", "Bye") → "HiByeByeHi"
+ *  <li>makeAbba("Yo", "Alice") → "YoAliceAliceYo"
+ *  <li>makeAbba("What", "Up") → "WhatUpUpWhat"
+ * </ul>
+ * 
+ * @author LZ-FSDev
+ * @see https://codingbat.com/prob/p161056
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public class MakeAbba {
+    public static void main(String[] args) {
+        System.out.println("makeAbba(\"Hi\", \"Bye\") -> " + makeAbba("Hi", "Bye"));
+        System.out.println("makeAbba(\"Yo\", \"Alice\") -> " + makeAbba("Yo", "Alice"));
+        System.out.println("makeAbba(\"What\", \"Up\") -> " + makeAbba("What", "Up"));
+    }
 
+    /**
+     * Given two strings, a and b, return the result of putting them together
+     * in the order abba, e.g. "Hi" and "Bye" returns "HiByeByeHi".
+     * 
+     * @param a First String.
+     * @param b Second String.
+     * @return A String in the order of abba.
+     * @since 0.0.1
+     */
+    public static String makeAbba(String a, String b) {
+        return a + b + b + a;
+    }    
+}
