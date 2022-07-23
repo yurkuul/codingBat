0e72d5e59a9ce54f9f43971764d9b04d48075b18
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-07-23 17:05:47 -0500

    Create XYBalance.java

---
+++ string_2/XYBalance.java
@@ -0,0 +1,52 @@
+/**
+ * We'll say that a String is xy-balanced if for all the 'x' chars in the
+ * string, there exists a 'y' char somewhere later in the string. So "xxy" is
+ * balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return true
+ * if the given string is xy-balanced.
+ * 
+ * <ul>
+ *  <li>xyBalance("aaxbby") → true
+ *  <li>xyBalance("aaxbb") → false
+ *  <li>xyBalance("yaaxbb") → false
+ * </ul>
+ * 
+ * @author LZ-FSDev
+ * @see https://codingbat.com/prob/p134250
+ * @since 17.0.1
+ * @version 0.0.1
+ */
+public class XYBalance {
+    public static void main(String[] args) {
+        System.out.println("xyBalance(\"aaxbby\") -> " + xyBalance("aaxbby"));
+        System.out.println("xyBalance(\"aaxbb\") -> " + xyBalance("aaxbb"));
+        System.out.println("xyBalance(\"yaaxbb\") -> " + xyBalance("yaaxbb"));
+    }
+
+    /**
+     * We'll say that a String is xy-balanced if for all the 'x' chars in the
+     * string, there exists a 'y' char somewhere later in the string. So "xxy"
+     * is balanced, but "xyx" is not. One 'y' can balance multiple 'x's. Return
+     * true if the given string is xy-balanced.
+     * 
+     * @param str A String.
+     * @return true if an "x" is followed by a "y" later in 'str'.
+     * @since 0.0.1
+     */
+    public static boolean xyBalance(String str) {
+        boolean balanced = false;
+        boolean hasX = false;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.substring(i,i+1).equals("x")) {
+                hasX = true;
+                balanced = false;
+            }
+            if (hasX && str.substring(i,i+1).equals("y")) {
+                balanced = true;
+            }
+        }
+        if (!hasX) {
+            balanced = true;
+        }
+        return balanced;
+    }    
+}
