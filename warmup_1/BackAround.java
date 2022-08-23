1756635ae141d18dcc1b068aecd7c27da68fe388
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-08-22 22:05:12 -0500

    Update BackAround.java

--- warmup_1/BackAround.java
+++ warmup_1/BackAround.java
@@ -11,13 +11,15 @@
  * @author LZ-FSDev
  * @see https://codingbat.com/prob/p161642
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 public class BackAround {
     public static void main(String[] args) {
         System.out.println("backAround(\"cat\") -> " + backAround("cat"));
         System.out.println("backAround(\"Hello\") -> " + backAround("Hello"));
         System.out.println("backAround(\"a\") -> " + backAround("a"));
+        System.out.println("backAround(\"test \") -> " + backAround("test "));
+        System.out.println("backAround(\"\") -> " + backAround(""));
     }
 
     /**
@@ -28,9 +30,12 @@ public class BackAround {
      * @param str used to return a new string with it's ending char at the front
      * and end of itself
      * @return String with str sandwiched between the last character of str
-     * @since 0.0.1
+     * @since 0.0.2
      */
     public static String backAround(String str) {
+        if (str.length() < 1) {
+            return "";
+        }
         return str.substring(str.length()-1) + str + str.substring(str.length()-1);
     }    
 }
