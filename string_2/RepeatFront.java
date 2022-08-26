f414e65389688f899b7d8455a568c800d4f90ed0
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-08-25 21:44:35 -0500

    Update RepeatFront.java

--- string_2/RepeatFront.java
+++ string_2/RepeatFront.java
@@ -12,13 +12,18 @@
  * @author LZ-FSDev
  * @see https://codingbat.com/prob/p128796
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 public class RepeatFront {
     public static void main(String[] args) {
-        System.out.println("repeatFront(\"Chocolate\", 4) -> " + repeatFront("Chocolate", 4));
-        System.out.println("repeatFront(\"Chocolate\", 3) -> " + repeatFront("Chocolate", 3));
-        System.out.println("repeatFront(\"Ice Cream\", 2) -> " + repeatFront("Ice Cream", 2));
+        System.out.println("repeatFront(\"Chocolate\", 4) -> "
+                           + repeatFront("Chocolate", 4));
+        System.out.println("repeatFront(\"Chocolate\", 3) -> "
+                           + repeatFront("Chocolate", 3));
+        System.out.println("repeatFront(\"Ice Cream\", 2) -> "
+                           + repeatFront("Ice Cream", 2));
+        System.out.println("repeatFront(\"\", 0) -> "
+                           + repeatFront("", 0));
     }
 
     /**
@@ -27,9 +32,10 @@ public class RepeatFront {
      * may assume that n is between 0 and the length of the string, inclusive
      * (i.e. n >= 0 and n <= str.length()).
      * 
-     * @param str the String used for repeating the front of it int n amount of times with 1 less character each time
-     * @param n the starting length of String str for repeating the front n times
-     * @return String that has been repeated n times with 1 character less each time
+     * @param str the String used for repeating the front of it int n amount of times
+     * with 1 less character each time.
+     * @param n the starting length of String str for repeating the front n times.
+     * @return String that has been repeated n times with 1 character less each time.
      * @since 0.0.1
      */
     public static String repeatFront(String str, int n) {
