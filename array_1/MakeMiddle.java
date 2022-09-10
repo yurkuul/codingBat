14d041a9827a46deacae6816b05707c62fd399b4
LZ-FSDev <95185447+yurkuul@users.noreply.github.com>
2022-09-09 21:16:07 -0500

    Update MakeMiddle.java

--- array_1/MakeMiddle.java
+++ array_1/MakeMiddle.java
@@ -13,16 +13,25 @@ import java.util.Arrays;
  * @author LZ-FSDev
  * @see https://codingbat.com/prob/p199519
  * @since 17.0.1
- * @version 0.0.1
+ * @version 0.0.2
  */
 public class MakeMiddle {
     public static void main(String[] args) {
         int[] array1 = {1, 2, 3, 4};
         int[] array2 = {7, 1, 2, 3, 4, 9};
         int[] array3 = {1, 2};
-        System.out.println("makeMiddle([1, 2, 3, 4]) -> " + Arrays.toString(makeMiddle(array1)));
-        System.out.println("makeMiddle([7, 1, 2, 3, 4, 9]) -> " + Arrays.toString(makeMiddle(array2)));
-        System.out.println("makeMiddle([1, 2]) -> " + Arrays.toString(makeMiddle(array3)));
+        int[] array4 = {0, 0, 0, 0};
+        int[] array5 = {10, 7, -4, 1};
+        System.out.println("makeMiddle(" + Arrays.toString(array1) + ") -> "
+                           + Arrays.toString(makeMiddle(array1)));
+        System.out.println("makeMiddle(" + Arrays.toString(array2) + ") -> "
+                           + Arrays.toString(makeMiddle(array2)));
+        System.out.println("makeMiddle(" + Arrays.toString(array3) + ") -> "
+                           + Arrays.toString(makeMiddle(array3)));
+        System.out.println("makeMiddle(" + Arrays.toString(array4) + ") -> "
+                           + Arrays.toString(makeMiddle(array4)));
+        System.out.println("makeMiddle(" + Arrays.toString(array5) + ") -> "
+                           + Arrays.toString(makeMiddle(array5)));
     }
 
     /**
@@ -30,8 +39,8 @@ public class MakeMiddle {
      * the middle two elements from the original array. The original array will be
      * length 2 or more.
      * 
-     * @param nums the array inputted to find the middle 2 elements
-     * @return int[] new array with the middle 2 elements of nums
+     * @param nums The array inputted to find the middle 2 elements.
+     * @return New integer array with the middle 2 elements of 'nums'.
      * @since 0.0.1
      */
     public static int[] makeMiddle(int[] nums) {
